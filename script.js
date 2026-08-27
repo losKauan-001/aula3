c4392185db705d964e55d4b4adc5d36a4a051a95
EA - Kauã Argolo Guilherme Oliveira <kaua_argolo@educadventista.org>
2026-08-27 07:25:19 -0300

    sec

--- script.js
+++ script.js
@@ -9,41 +9,48 @@ const paragrafoResultado = document.getElementById("resultado")
 button.addEventListener("click", calcularTroco)
 
 function calcularTroco(){
+
     const preco = prec.valueAsNumber
     const qtd = qt.valueAsNumber
     const DR = dr.valueAsNumber
 
-    if (preco.value === "" || preco <= 0){
+    if (preco === ""){
         alert("digite um preço valido")
         return
     }
 
     if (qtd > 0){
+
         const valorTotal = (preco * qtd)
         const valorDoTroco = -(valorTotal - DR)
         const valorDoTrocoConvertido = valorDoTroco.toLocaleString("pt-BR", {style: "currency", currency: "BRL"})
         
-        if (valorDoTroco > 0)
-        paragrafoResultado.textContent = `
-            Troco = ${valorDoTrocoConvertido}
-        `
-        else if (qtd < 0) {
+        if (valorDoTroco > 0){
+
+            paragrafoResultado.textContent = `
+                Troco = ${valorDoTrocoConvertido}
+            `
+
+        } else if (valorDoTroco < 0) {
+
             let local = -(valorDoTroco)
             let local2 = local.toLocaleString("pt-BR", {style: "currency", currency: "BRL"})
 
             paragrafoResultado.textContent = `
-            Dinheiro insuficiente. Faltam = ${local2}
-        `
+                Dinheiro insuficiente. Faltam = ${local2}
+            `
+
         } else {
             paragrafoResultado.textContent = `
                 Sem troco, obrigado
             `
         }
+
     } else {
         paragrafoResultado.textContent = `
             digite um valor valido
         `
     }
 
-    preco.value = ""
+    prec.value = ""
 }
